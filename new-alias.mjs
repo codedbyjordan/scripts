7d0aaf064ac5e2b6fa3f18e56ee6c3b025c745be
Jordan Baron <jordannbaron@gmail.com>
2022-03-24 11:22:22 -0400

    Add $.verbose instead of wrapping everything w/quiet()

--- new-alias.mjs
+++ new-alias.mjs
@@ -1,6 +1,8 @@
 #!/usr/bin/env zx
 import exists from "./helpers/exists.mjs"
 
+$.verbose = false;
+
 const aliasName = process.argv[3];
 const aliasCommand = process.argv[4];
 const alias = `alias ${aliasName}="${aliasCommand}"`;
@@ -12,17 +14,17 @@ const ohMyZsh = process.env.ZSH ? true : false;
 if (ohMyZsh) {
   aliasesFile = `${process.env.ZSH}\/custom\/aliases.zsh`;
   const aliasFileExists = await exists(aliasesFile);
-  if (!aliasFileExists) await quiet($`touch ${aliasesFile}`);
+  if (!aliasFileExists) await $`touch ${aliasesFile}`;
 }
 
 if (argv._.length <= 1) {
   console.log("Command usage: new-alias [name] [command]");
 } else {
   try {
-    await quiet($`cat ${aliasesFile} | grep ${alias}`);
+    await $`cat ${aliasesFile} | grep ${alias}`;
     console.log(chalk.yellow("Alias already exists, so it was not added"));
   } catch (e) {
-    await quiet($`echo ${alias} >> ${aliasesFile}`);
+    await $`echo ${alias} >> ${aliasesFile}`;
     console.log(chalk.green(`Success! Alias ${aliasName} was successfully added.`))
   }
 }
