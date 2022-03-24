62819845cc3473c1841e5ad3f3148cac54d4934b
Jordan Baron <jordannbaron@gmail.com>
2022-03-23 22:08:55 -0400

    Update install script to be a bit verbose

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+echo Installing zx...
+
+npm install -g zx > /dev/null
+
+echo Installing scripts...
 mkdir $HOME/.scripts/
 mkdir $HOME/.scripts/helpers
 echo "export PATH=${PATH}:${HOME}/.scripts" >> ~/.zshrc
 
+# remove all file extensions so scripts can be ran as 'example' instead of 'example.mjs'
 for file in *.mjs; do
     cp -- "$file" "$HOME/.scripts/${file%%.mjs}"
 done
 
 cp helpers/*.mjs $HOME/.scripts/helpers
 source $HOME/.zshrc
+
+echo Done\!
